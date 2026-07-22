c2f4f965b0acdbf7823520c29933379f7ad2dcee
Justin Gionet <jgionet@innovmetric.com>
2026-07-22 19:18:03 +0000

    try applying state without use_oidc

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ terraform {
     container_name       = "s-terraform-im-jg-testing-web-application-infra-2-dev"
     key                  = "web-application-infra-2.tfstate"
     use_azuread_auth     = true
-    use_oidc             = true
   }
 }
 

474b3b43c7ca5e03bbc1168b1a93f5946a6c94a0
Justin Gionet <jgionet@innovmetric.com>
2026-07-23 13:51:28 +0000

    removed things that were used for azure backend

--- main.tf
+++ main.tf
@@ -1,29 +1,9 @@
 terraform {
   required_version = ">= 1.0.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    storage_account_name = "stjgdevequipeinterne"
-    container_name       = "s-terraform-im-jg-testing-web-application-infra-2-dev"
-    key                  = "web-application-infra-2.tfstate"
-    use_azuread_auth = true
-    use_oidc = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true # Matches the backend authentication
 }
 
 # An in-memory null resource that does not create physical infrastructure
-resource "null_resource" "backend_test" {
+resource "null_resource" "main_test" {
   triggers = {
     test_run_time = timestamp()
   }
@@ -34,6 +14,6 @@ module "external_module" {
 }
 
 output "backend_status" {
-  value       = "Success! The backend configuration is active and working."
+  value       = "Success! The external module configuration is active and working."
   description = "A simple string confirmation to prove execution completed."
 }
